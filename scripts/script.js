af666536da587badb33552beb884427ead74a4f4
helgesonSE <140814418+helgesonSE@users.noreply.github.com>
2023-11-12 23:25:49 +0100

    Added local storage loading/writing

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
-let showList = [];
-let searchList = [];
+let showArray = getData();
+let searchArray = [];
 const submitShowButton = document.getElementById("submitShow")
 submitShowButton.addEventListener("click", addShow);
 const addShowInput = {
@@ -23,12 +23,28 @@ function setView (button) {
     document.getElementById(button).style.display = "block";
 }
 
-function addShow(){
-    showList.push({
+function addShow (){
+    showArray.push({
         title:addShowInput.title.value, 
         genre:addShowInput.genre.value,
         rating:addShowInput.rating.value, 
         description:addShowInput.description.value});
+        saveData();
+    }
+    
+function saveData () {
+    console.log(showArray);
+    let savedShows = getData();
+    savedShows = showArray ? showArray : []; //om det finns data sedan innan, konvertera från JSON till objekt, annars skapa en tom array.
+    console.log(savedShows);
+    localStorage.setItem('savedData', JSON.stringify(savedShows));//gör om data till sträng, spara i local
+}
+
+function getData () {
+    let savedShows = localStorage.getItem('savedData');
+    savedShows = JSON.parse(savedShows);
+    console.log(savedShows);
+    return savedShows ? savedShows : [];
 }
 
 
@@ -37,27 +53,27 @@ function addShow(){
 
 
 
-showList.push({title:"Låtsatstitel", genre:"Humor", rating:"Barnförbjuden", 
+showArray.push({title:"Låtsatstitel", genre:"Humor", rating:"Barnförbjuden", 
     description:"Handlar om folk som äter frukost"});
-showList.push({title:"Låtsatstitel2", genre:"Drama", rating:"Barntillåten", 
+showArray.push({title:"Låtsatstitel2", genre:"Drama", rating:"Barntillåten", 
     description:"Handlar om folk som äter middag"});
 
 var templateDiv = document.getElementById("template");
 
 var newShowElement = templateDiv.cloneNode(true);
 
-newShowElement.querySelector("h2").textContent = showList[0].title;
-newShowElement.querySelector("genre").textContent = showList[0].genre;
-newShowElement.querySelector("rating").textContent = showList[0].rating;
-newShowElement.querySelector("p").textContent = showList[0].description;
+newShowElement.querySelector("h2").textContent = showArray[0].title;
+newShowElement.querySelector("genre").textContent = showArray[0].genre;
+newShowElement.querySelector("rating").textContent = showArray[0].rating;
+newShowElement.querySelector("p").textContent = showArray[0].description;
 
 document.getElementById("listShow").appendChild(newShowElement);
 
 var newShowElement = templateDiv.cloneNode(true);
 
-newShowElement.querySelector("h2").textContent = showList[1].title;
-newShowElement.querySelector("genre").textContent = showList[1].genre;
-newShowElement.querySelector("rating").textContent = showList[1].rating;
-newShowElement.querySelector("p").textContent = showList[1].description;
+newShowElement.querySelector("h2").textContent = showArray[1].title;
+newShowElement.querySelector("genre").textContent = showArray[1].genre;
+newShowElement.querySelector("rating").textContent = showArray[1].rating;
+newShowElement.querySelector("p").textContent = showArray[1].description;
 
 document.getElementById("listShow").appendChild(newShowElement);
